da4a2f16958c21ad560f00e3154234853d9f0cde
Ardhi Solikhin <asolikhin@fintax.id>
2023-08-07 23:32:04 +0700

    up

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container class="pt-0 pb-0">
+        <Header />
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+}
+</script>
